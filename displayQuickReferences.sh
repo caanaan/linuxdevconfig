602ccd4abc0673db4829aef6cdd0d4067d73328e
Luke Thomas <luke.thomas@allegion.com>
2016-03-15 10:20:43 -0400

    Updated to create quick references to pdf via script.

--- displayQuickReferences.sh
+++ displayQuickReferences.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 # NOTE - this requires GRIP (https://github.com/joeyespo/grip) to be installed!
+# and wkhtmltopdf:
+#  sudo apt-get install wkhtmltopdf
+#  sudo ln -s /usr/bin/wkhtmltopdf /usr/local/bin/html2pdf
 
 grip ./vim/MyVimQuickReference.md 8200 &
-/opt/google/chrome/chrome http://localhost:8200 &
+sleep 1
+#/opt/google/chrome/chrome http://localhost:8200 &
+html2pdf http://localhost:8200 VimQuickReference.pdf
+killall grip
 
-grip ./zsh/MyZshQuickReference.md 8201 &
-/opt/google/chrome/chrome http://localhost:8201 &
-
-#NOTE - you will need to kill the grip instances manually when done!
-# Typically using killall grip
+grip ./zsh/MyZshQuickReference.md 8200 &
+sleep 1
+#/opt/google/chrome/chrome http://localhost:8201 &
+html2pdf http://localhost:8200 ZshQuickReference.pdf
+killall grip
