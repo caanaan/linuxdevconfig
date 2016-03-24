777b705d59e2e586d0fed89a5ae526fdddebf381
Luke Thomas <luke.thomas@allegion.com>
2016-03-24 10:05:21 -0400

    Updated doc name.

--- createQuickReferences.sh
+++ createQuickReferences.sh
@@ -4,13 +4,13 @@
 #  sudo apt-get install wkhtmltopdf
 #  sudo ln -s /usr/bin/wkhtmltopdf /usr/local/bin/html2pdf
 
-grip ./vim/MyVimQuickReference.md 8200 &
+grip ./vim/VimQuickReference.md 8200 &
 sleep 1
 #/opt/google/chrome/chrome http://localhost:8200 &
 html2pdf http://localhost:8200 VimQuickReference.pdf
 killall grip
 
-grip ./zsh/MyZshQuickReference.md 8200 &
+grip ./zsh/ZshQuickReference.md 8200 &
 sleep 1
 #/opt/google/chrome/chrome http://localhost:8201 &
 html2pdf http://localhost:8200 ZshQuickReference.pdf
