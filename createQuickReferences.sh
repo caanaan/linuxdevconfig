d847dd63c4d3bda1c34b8bc95eedf2281e90a35f
Luke Thomas <luke.thomas@allegion.com>
2016-04-13 13:40:32 +0000

    Updated quick reference creation.

--- createQuickReferences.sh
+++ createQuickReferences.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# NOTE - this requires GRIP (https://github.com/joeyespo/grip) to be installed!
+# NOTE - this requires GRIP (https://github.com/joeyespo/grip) to be installed
+#  sudo pip install grip
 # and wkhtmltopdf:
 #  sudo apt-get install wkhtmltopdf
 #  sudo ln -s /usr/bin/wkhtmltopdf /usr/local/bin/html2pdf
+# NOTE - wkhtmltopdf requires an X server, so won't work if none are running...
 
 grip ./vim/VimQuickReference.md 8200 &
 sleep 1
